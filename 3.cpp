bc1fa257f89aa1bb87e4b14c3526957913bf8012
Ramil-caramel <ramilrr2706@gmail.com>
2024-10-27 19:52:56 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include "header.hpp"
+size_t CountTwoness(const std::vector<Student> &student_v){
+	size_t chislo= 0;
+
+	for (int i = 0; i < student_v.size(); ++i){
+		for (int j = 0; j < student_v[i].Ratings.size();++j){
+			if (student_v[i].Ratings[j] == 2){
+				++chislo;
+			}
+		}
+	}
+	return chislo;
+}
