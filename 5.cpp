135d4f72f12e2ccd242fef98e638f8d914475356
Ramil-caramel <ramilrr2706@gmail.com>
2024-11-09 16:48:56 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -3,8 +3,10 @@
 std::vector<Student> VectorMathExcellent(const std::vector<Student>& student_v) {
 	std::vector<Student> vec;
 	for (int i = 0; i < student_v.size(); ++i) {
-		if (student_v[i].Ratings[1] == 5) {
-			vec.push_back(student_v[i]);
+		for (int j = 0; j < student_v[i].Subjects.size(); ++j) {
+			if (student_v[i].Subjects[j] == "Math" and student_v[i].Ratings[j] == 5) {
+				vec.push_back(student_v[i]);
+			}
 		}
 	}
 	return vec;
